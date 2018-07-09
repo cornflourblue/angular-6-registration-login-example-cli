bf7e9e0cc697bc6f523ed9fd312a783ef36b3639
Jason Watmore <jason.watmore@hotmail.com>
2018-07-09 18:34:56 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# angular-6-registration-login-example-cli
+
+Angular 6 - User Registration and Login Example with Angular CLI
+
+To see a demo and further details go to http://jasonwatmore.com/post/2018/05/16/angular-6-user-registration-and-login-example-tutorial
+
---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,28 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class AuthenticationService {
+    constructor(private http: HttpClient) { }
+
+    login(username: string, password: string) {
+        return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username: username, password: password })
+            .pipe(map(user => {
+                // login successful if there's a jwt token in the response
+                if (user && user.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                }
+
+                return user;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+    }
+}
